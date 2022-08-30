845d7ec6a02bc1ccbddf13ef77b0063456bf0e0e
Vinicius-SO <vhisoares39@gmail.com>
2022-08-30 19:46:52 -0300

    Theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { ThemeProvider } from 'styled-components'
+
 import { Button } from './components/Button'
+import { defaultTheme } from './styles/theme/default'
 
 export function App() {
     return (
-      <><Button variant='danger' /><Button variant='secondary' /><Button /><Button /></>
+      <>
+          <ThemeProvider theme={defaultTheme}>
+            <Button variant='danger' />
+            <Button variant='secondary' />
+            <Button />
+            <Button />
+          </ThemeProvider>
+      </>
     )
 }
 
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,8 +17,13 @@ const buttonVariants = {
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
-    height: 100px;
+    height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => css`background-color: ${buttonVariants[props.variant]}`}
+    background: ${props=> props.theme.primary};
+
+    /* ${props => css`background-color: ${buttonVariants[props.variant]}`} */
 
 `
