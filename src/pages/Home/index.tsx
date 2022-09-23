305405f1466c6400b91723e5259a6f57fd449aa5
Vinicius-SO <vhisoares39@gmail.com>
2022-09-22 21:51:58 -0300

    Criando countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,9 +26,8 @@ interface Cycle {
 export function Home(){
 
     const [ cycles, setCycles ] = useState<Cycle[]>([])
-
     const [ activeCycleId, setActiveCycleId ] = useState<string | null>(null)
-
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
     const {register, handleSubmit, watch, reset} = useForm<newCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
@@ -41,6 +40,15 @@ export function Home(){
     
     const activeCycle = cycles.find( cycle=> cycle.id === activeCycleId )
     
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 8 
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60) 
+    const secondsAmount = currentSeconds % 60
+   
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
     console.log(activeCycle)
 
     const task = watch('task')
@@ -48,7 +56,7 @@ export function Home(){
 
     function handleCreateNewCicle(data:newCycleFormData){
         const id = String (new Date().getTime())
-        
+
         const newCycle: Cycle = {
             id,
             task: data.task,
@@ -93,11 +101,11 @@ export function Home(){
                     <span>minutos</span>
                 </FormContainer>
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
                 <StartCountdownButton type="submit" disabled={isSubmitDisabled} > <Play size={24}/> Começar</StartCountdownButton>
